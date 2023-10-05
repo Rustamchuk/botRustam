cc5cba23e6e91752261135b79d8d08a6942b09f7
Рустам Назаров <61919245+Rustamchuk@users.noreply.github.com>
2023-10-05 21:44:28 +0300

    ProjectArchitecture

--- cmd/bot/main.go
+++ cmd/bot/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/Rustamchuk/botRustam/internal/app/commands"
 	"github.com/Rustamchuk/botRustam/internal/service/product"
 	"log"
 	"os"
@@ -32,6 +33,8 @@ func main() {
 
 	productService := product.NewService()
 
+	commander := commands.NewCommander(bot, productService)
+
 	for update := range updates {
 		if update.Message == nil {
 			continue
@@ -41,35 +44,11 @@ func main() {
 
 		switch update.Message.Command() {
 		case "help":
-			helpCommand(bot, update.Message)
+			commander.Help(update.Message)
 		case "list":
-			listCommand(bot, update.Message, productService)
+			commander.List(update.Message)
 		default:
-			defaultBehavior(bot, update.Message)
+			commander.Default(update.Message)
 		}
 	}
-}
-
-func listCommand(bot *tgbotapi.BotAPI, inputMessage *tgbotapi.Message, productService *product.Service) {
-	outMessage := "All products\n\n"
-	products := productService.List()
-	for _, p := range products {
-		outMessage += p.Title
-		outMessage += "\n"
-	}
-	msg := tgbotapi.NewMessage(inputMessage.Chat.ID, outMessage)
-	bot.Send(msg)
-}
-
-func helpCommand(bot *tgbotapi.BotAPI, inputMessage *tgbotapi.Message) {
-	msg := tgbotapi.NewMessage(inputMessage.Chat.ID,
-		"/help - command list\n"+
-			"/list - product list")
-	bot.Send(msg)
-}
-
-func defaultBehavior(bot *tgbotapi.BotAPI, inputMessage *tgbotapi.Message) {
-	msg := tgbotapi.NewMessage(inputMessage.Chat.ID, "You wrote "+inputMessage.Text)
-	//msg.ReplyToMessageID = update.Message.MessageID
-	bot.Send(msg)
 }
---
+++ internal/app/commands/list.go
@@ -0,0 +1,14 @@
+package commands
+
+import tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+
+func (c *Commander) List(inputMessage *tgbotapi.Message) {
+	outMessage := "All products\n\n"
+	products := c.productService.List()
+	for _, p := range products {
+		outMessage += p.Title
+		outMessage += "\n"
+	}
+	msg := tgbotapi.NewMessage(inputMessage.Chat.ID, outMessage)
+	c.bot.Send(msg)
+}
