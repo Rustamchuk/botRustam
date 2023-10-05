93356c518ae55484c85891aa565a74116fb40e46
Рустам Назаров <61919245+Rustamchuk@users.noreply.github.com>
2023-10-05 20:44:21 +0300

    botStartLive

--- cmd/bot/main.go
+++ cmd/bot/main.go
@@ -1,7 +1,40 @@
-package bot
+package main
 
-import "fmt"
+import (
+	"log"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+)
+
+const TOKEN = "5970472266:AAGDeQx-uS-hXqgYKyu2pK8eo9qBpdg5C_8"
 
 func main() {
-	fmt.Println("Hello")
+	bot, err := tgbotapi.NewBotAPI(TOKEN)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	bot.Debug = true
+
+	log.Printf("Authorized on account %s", bot.Self.UserName)
+
+	u := tgbotapi.UpdateConfig{
+		Timeout: 60,
+	}
+	u.Timeout = 60
+
+	updates := bot.GetUpdatesChan(u)
+
+	for update := range updates {
+		if update.Message == nil { // If we got a message
+			continue
+		}
+
+		log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+
+		msg := tgbotapi.NewMessage(update.Message.Chat.ID, "You wrote "+update.Message.Text)
+		//msg.ReplyToMessageID = update.Message.MessageID
+
+		bot.Send(msg)
+	}
 }
